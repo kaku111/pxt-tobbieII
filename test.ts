c157fe1ba0d88374d9ac51e6acaf90083b918317
kaku111 <35320401+kaku111@users.noreply.github.com>
2019-03-15 22:16:25 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
+basic.forever(function () {
+    TobbieII.forward()
+    if (TobbieII.RBlock(512)) {
+        TobbieII.stopwalk()
+    }
+})
